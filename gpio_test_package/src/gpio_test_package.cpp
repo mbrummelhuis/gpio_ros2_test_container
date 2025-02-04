274161c5e3a676c1379b65c590c08639d686f68d
Martijn Brummelhuis <36448450+mbrummelhuis@users.noreply.github.com>
2025-02-04 16:08:19 +0100

    Update gpio_test_package.cpp

--- gpio_test_package/src/gpio_test_package.cpp
+++ gpio_test_package/src/gpio_test_package.cpp
@@ -2,43 +2,38 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
 
-#define PIN_NUMBER 13
+#define PIN1_NUMBER 10
+#define PIN2_NUMBER 9
+#define PIN3_NUMBER 3
+#define PIN4_NUMBER 4
 
 class GPIOTestNode : public rclcpp::Node
 {
     public:
         GPIOTestNode() 
-        : Node("gpio_test_node"), state_(0)
+        : Node("gpio_test_node")
         {
-            publisher_ = this->create_publisher<std_msgs::msg::String>("press", 10);
             timer_ = this->create_wall_timer(50ms, std::bind(&GPIOTestNode::timer_callback, this));
         }
     private:
         void timer_callback()
         {
-            state_ = digitalRead(PIN_NUMBER);
-            auto message = std_msgs::msg::String();
-            message.data = "Button press result: %i", std::to_string(state_);
-            if (digitalRead(PIN_NUMBER)==LOW) {
-                RCLCPP_INFO(this->get_logger(), "Low");}
-            else if(digitalRead(PIN_NUMBER)==HIGH) {
-                RCLCPP_INFO(this->get_logger(), "High");}
-            publisher_->publish(message);
+            printf("Button press result: %d \t %d \t %d \t %d \n", digitalRead(PIN1_NUMBER), digitalRead(PIN2_NUMBER), digitalRead(PIN3_NUMBER), digitalRead(PIN4_NUMBER));
         }
         rclcpp::TimerBase::SharedPtr timer_;
-        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-        int state_;
 };
 
 int main(int argc, char * argv[])
 {
     // Set up wiringOP
     wiringPiSetup();
-    pinMode(PIN_NUMBER, INPUT);
+    pinMode(PIN1_NUMBER, INPUT);
+    pinMode(PIN2_NUMBER, INPUT);
+    pinMode(PIN3_NUMBER, INPUT);
+    pinMode(PIN4_NUMBER, INPUT);
     // Run node
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<GPIOTestNode>());
